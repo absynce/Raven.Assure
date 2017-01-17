030e6411b897f95384951d12e78a99a57d7c084b
Jared M. Smith <absynce@gmail.com>
2017-01-17 12:11:13 -0600

    Initial support for gulp build
    Can run gulp build to build Raven.Assure.
    
    Next step: move folder structure to standard... src/Raven.Assure and
    test/Raven.Assure.Test.

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,38 @@ const path = require('path');
 const proc = require('child_process');
 const usage = require('gulp-help-doc');
 
+// Define reusable loggers.
+
+var toString = (msg) => msg.toString();
+// dotnet CLI adds line break, so when combined with log it adds an additional empty line.
+// This replaces that line break with an empty string.
+var removeLineBreaks = (msg) => msg.replace(/(\r\n|\r|\n)/g, '');
+var logDatar = _.flow(toString, removeLineBreaks, gutil.log);
+var logError = _.flow(gutil.colors.red, logDatar);
+
 gulp.task('help', () => usage(gulp));
 
 /**
+ * Build/compile Raven.Assure.
+ * Same as `dotnet build /src/Raven.Assure/`.
+ *
+ * @task {build}
+ */
+gulp.task('build', function buildRavenAssure(done) {
+   var buildProcess = proc
+      .spawn('dotnet', ['build'], {
+         cwd: path.join('src', 'Raven.Assure')
+      });
+
+   buildProcess.stdout.on('data', logDatar);
+   buildProcess.stderr.on('data', logError);
+   buildProcess.on('close', done);
+});
+
+/**
  * Run Raven.Assure.Test xUnit tests.
- * Same as `dotnet test src/Raven.Assure.Test/`
+ * Same as `dotnet test src/Raven.Assure.Test/`.
+ *
  * @task {test}
  */
 gulp.task('test', function testRavenAssure(done) {
@@ -19,12 +46,6 @@ gulp.task('test', function testRavenAssure(done) {
          cwd: path.join('src', 'Raven.Assure.Test')
       });
 
-   // dotnet CLI adds line break, so when combined with log it adds an additional empty line.
-   // This replaces that line break with an empty string.
-   var removeLineBreaks = (msg) => msg.replace(/(\r\n|\r|\n)/g, '');
-   var toString = (msg) => msg.toString();
-   var logDatar = _.flow(toString, removeLineBreaks, gutil.log);
-   var logError = _.flow(gutil.colors.red, logDatar);
 
    testProcess.stdout.on('data', logDatar);
    testProcess.stderr.on('data', logError);
