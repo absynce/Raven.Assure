b2dbd94e73fd4751cf703d29d33b7622ebba8764
Jared M. Smith <absynce@gmail.com>
2017-01-17 11:50:41 -0600

    Add gulp test help info via JSdoc comment
    Next step: implement gulp app/build/compile.

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,25 @@ const _ = require('lodash/fp');
 const args = require('yargs').argv;
 const gulp = require('gulp');
 const gutil = require('gulp-util');
-// const log = gutil.log;
 const path = require('path');
 const proc = require('child_process');
 const usage = require('gulp-help-doc');
 
-gulp.task('help', function() { return usage(gulp); });
+gulp.task('help', () => usage(gulp));
 
-/*
- TODO: Document this.
+/**
+ * Run Raven.Assure.Test xUnit tests.
+ * Same as `dotnet test src/Raven.Assure.Test/`
+ * @task {test}
  */
 gulp.task('test', function testRavenAssure(done) {
-   debugger;
    var testProcess = proc
       .spawn('dotnet', ['test'], {
          cwd: path.join('src', 'Raven.Assure.Test')
       });
 
    // dotnet CLI adds line break, so when combined with log it adds an additional empty line.
+   // This replaces that line break with an empty string.
    var removeLineBreaks = (msg) => msg.replace(/(\r\n|\r|\n)/g, '');
    var toString = (msg) => msg.toString();
    var logDatar = _.flow(toString, removeLineBreaks, gutil.log);
