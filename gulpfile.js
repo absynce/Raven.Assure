54ef2af0a64bea6a2a715c16d90757fdf505392f
Jared M. Smith <absynce@gmail.com>
2017-01-18 16:53:19 -0600

    Add gulp package
    Similar to mobile, but the target is net46. No argument for others,
    since no others are supported (yet).

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
 const _ = require('lodash/fp');
 const args = require('yargs').argv;
+const fs = require('fs');
 const gulp = require('gulp');
 const gutil = require('gulp-util');
 const path = require('path');
 const proc = require('child_process');
 const usage = require('gulp-help-doc');
 
-// Define reusable loggers.
-
-var toString = (msg) => msg.toString();
-// dotnet CLI adds line break, so when combined with log it adds an additional empty line.
-// This replaces that line break with an empty string.
-var removeLineBreaks = (msg) => msg.replace(/(\r\n|\r|\n)/g, '');
-var logDatar = _.flow(toString, removeLineBreaks, gutil.log);
-var logError = _.flow(gutil.colors.red, logDatar);
+var paths = {
+  bin: {
+    prefix: 'src/Raven.Assure/bin',
+    environment: 'Release',
+    target: 'net46',
+    files: '**/*'
+  },
+  package: 'package'
+};
 
 gulp.task('help', () => usage(gulp));
 
@@ -51,3 +53,33 @@ gulp.task('test', function testRavenAssure(done) {
    testProcess.stderr.on('data', logError);
    testProcess.on('close', done);
 });
+
+/**
+ * Package Raven.Assure CLI into ./package/{target}/smuggle-{version}-{environment}.
+ * @task {package}
+ * @arg {environment} The build environment: {Debug|Release(default)}
+ *
+ * TODO: add branch to package output folder name, like with mobile.
+ */
+gulp.task('package', function packageRavenAssure(done) {
+  const smugglePackage = getPackageJson();
+  var env = args.environment || paths.bin.environment;
+  var fullPackagePath = path.join(paths.package, paths.bin.target, `smuggle-${smugglePackage.version}-${env}`);
+  var fullBinPath = path.join(paths.bin.prefix, env, paths.bin.target, paths.bin.files);
+
+  gutil.log('Packaging from:', gutil.colors.cyan(fullBinPath));
+  gutil.log('Packaging into:', gutil.colors.cyan(fullPackagePath));
+
+  gulp.src(fullBinPath)
+      .pipe(gulp.dest(fullPackagePath));
+});
+
+// Define reusable loggers.
+var toString = (msg) => msg.toString();
+// dotnet CLI adds line break, so when combined with log it adds an additional empty line.
+// This replaces that line break with an empty string.
+var removeLineBreaks = (msg) => msg.replace(/(\r\n|\r|\n)/g, '');
+var logDatar = _.flow(toString, removeLineBreaks, gutil.log);
+var logError = _.flow(gutil.colors.red, logDatar);
+
+var getPackageJson = () => JSON.parse(fs.readFileSync('./package.json', 'utf8'));
