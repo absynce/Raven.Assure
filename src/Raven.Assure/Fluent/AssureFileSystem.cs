06327f4c8fbecdb3d2f08661ba5b2393a24f4dca
Jared M. Smith <absynce@gmail.com>
2017-02-03 15:10:38 -0600

    Add tests for file system
    Next step: abstract resource document (database.document,
    filesystem.document) handler.

---
+++ src/Raven.Assure/Fluent/AssureFileSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.IO.Abstractions;
+using Raven.Assure.Log;
+
+namespace Raven.Assure.Fluent
+{
+   public class AssureFileSystem : IAssureFileSystem, ISetupAssure<AssureFileSystem>
+   {
+      protected IFileSystem fileSystem;
+      protected ILogger logger;
+
+      public string BackupLocation { get; protected set; }
+      public string FileSystemName { get; protected set; }
+      public string ServerUrl { get; protected set; }
+
+      public AssureFileSystem()
+      {
+         this.fileSystem = new FileSystem();
+         this.logger = new ConsoleLogger();
+      }
+
+      public AssureFileSystem LogWith(ILogger logger)
+      {
+         this.logger = logger;
+
+         return this;
+      }
+
+      public AssureFileSystem On(IFileSystem fileSystem)
+      {
+         this.fileSystem = fileSystem;
+
+         return this;
+      }
+   }
+}
